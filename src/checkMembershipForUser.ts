06332df6a9626471da0daa9de8bc5ff3470cf6bb
Droid-An <andrei.filippov.aaf@gmail.com>
2026-03-10 14:27:27 +0000

    add membership check

---
+++ src/checkMembershipForUser.ts
@@ -0,0 +1,20 @@
+import { Octokit } from "octokit";
+// import { octokit } from "./testApp.js";
+export async function checkMembershipForUser(
+  senderLogin: string,
+  octokit: Octokit,
+) {
+  const orgName = "CodeYourFuture";
+  try {
+    await octokit.request("GET /orgs/{org}/members/{username}", {
+      org: orgName,
+      username: senderLogin,
+    });
+    return true;
+  } catch (err: any) {
+    if (err.status === 404) return false;
+    throw err;
+  }
+}
+//command to see if user droid-an is a member of codeyourfuture
+// console.log(await checkMembershipForUser("Droid-An", octokit));

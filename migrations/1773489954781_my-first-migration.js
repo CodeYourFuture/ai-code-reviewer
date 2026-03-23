5a56229d24fb7dc4c186503f9b0a7a1f372b2cd0
Droid-An <andrei.filippov.aaf@gmail.com>
2026-03-23 15:27:40 +0000

    wip migration

--- migrations/1773489954781_my-first-migration.js
+++ migrations/1773489954781_my-first-migration.js
@@ -9,11 +9,22 @@ export const shorthands = undefined;
  * @returns {Promise<void> | void}
  */
 export const up = (pgm) => {
-  pgm.createTable("feedback", {
-    id: { type: "bigserial not null primary key", notNull: true },
-    feedback: "text",
+  pgm.createTable("prompts", {
+    id: { type: "bigserial primary key", notNull: true },
+    prompt: "text",
+  });
+
+  pgm.createTable("feedback_points", {
+    id: { type: "bigserial primary key", notNull: true },
+    ai_review: "text",
     commit_sha: { type: "varchar(50)", notNull: true },
-    model: "varchar",
+    llm_model: "varchar",
+    prompt_id: { type: "bigint", references: "prompts(id)" },
+  });
+
+  pgm.createTable("feedback", {
+    id: { type: "bigserial primary key", notNull: true },
+    review_id: { type: "bigint", references: "review(id)" },
   });
 };
 
@@ -22,4 +33,8 @@ export const up = (pgm) => {
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const down = (pgm) => {};
+export const down = (pgm) => {
+  pgm.dropColumn("feedback", "prompt_id");
+  pgm.dropTable("feedback");
+  pgm.dropTable("prompts");
+};

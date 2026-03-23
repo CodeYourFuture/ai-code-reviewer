0986faefadc06a8f76064fd09af2c7aa75bbe6e7
Droid-An <andrei.filippov.aaf@gmail.com>
2026-03-23 16:30:52 +0000

    comment out test ai reviewer to avoid build problems

--- src/utils/testAiReviewer.ts
+++ src/utils/testAiReviewer.ts
@@ -1,15 +1,15 @@
-import {
-  MODEL,
-  codeQualityPrompt,
-  commentQualityPrompt,
-  defaultChatParameters,
-  runAiReview,
-} from "../networks/ai_api_request.js";
-import { PRFile } from "../types/githubTypes.js";
-import data from "../utils/sampleOutput/output2.json" with { type: "json" };
-import { formAiOutputDataObject } from "./storeAiReviewData.js";
+// import {
+//   MODEL,
+//   codeQualityPrompt,
+//   commentQualityPrompt,
+//   defaultChatParameters,
+//   runAiReview,
+// } from "../networks/ai_api_request.js";
+// import { PRFile } from "../types/githubTypes.js";
+// import data from "../utils/sampleOutput/output2.json" with { type: "json" };
+// import { formAiOutputDataObject } from "./storeAiReviewData.js";
 
-const files: PRFile[] = data as PRFile[];
-const prompts = [codeQualityPrompt, commentQualityPrompt];
-const review = await runAiReview(files);
-formAiOutputDataObject(defaultChatParameters, MODEL, prompts, review);
+// const files: PRFile[] = data as PRFile[];
+// const prompts = [codeQualityPrompt, commentQualityPrompt];
+// const review = await runAiReview(files);
+// formAiOutputDataObject(defaultChatParameters, MODEL, prompts, review);

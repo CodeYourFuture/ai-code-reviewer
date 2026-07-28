0a06d3a484a06a3d82a5ad9e23e6e8f3d21cf84d
Andrei Filippov <andrei.filippov.aaf@gmail.com>
2026-07-28 20:30:37 +0100

    Send out comments and add tests (#51)
    This pr:
    - extracts the comment post logic and test it with unit test
    - upload variables to test env
    - fix error code assignment instead of comparison in `server.ts`
    - fixes test in validate feedback points

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,10 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
 
 export default defineConfig({
   test: {
+    env: loadEnv("test", process.cwd(), ""),
     exclude: ["**/node_modules/**", "**/dist/**", "./temp/**", "tsconfig.json"],
   },
 });

bfd2433993b10184edd51efd98ee0aa1167bad66
Andrei Filippov <andrei.filippov.aaf@gmail.com>
2026-07-28 23:39:40 +0100

    make use of pull  connection check (#53)
    This pr extracts db connection check into a function and awaits it on
    the server listen, so that server won't start listening without db
    running

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,12 @@
 import { Pool } from "pg";
 import { env } from "../config/env.js";
 
-const pool = new Pool({
-  connectionString: env.DATABASE_URL,
-});
+const pool = new Pool({ connectionString: env.DATABASE_URL });
 
-pool
-  .query("SELECT NOW()")
-  .then(() => console.log("Connected to PostgreSQL!"))
-  .catch((err) => console.error("Connection error:", err));
+export async function assertDbConnection() {
+  await pool
+    .query("SELECT NOW()")
+    .then(() => console.log("Connected to PostgreSQL!"));
+}
 
 export default pool;
